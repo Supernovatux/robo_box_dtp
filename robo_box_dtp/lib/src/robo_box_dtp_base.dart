5e9748ba307494a78491f4190e8ca1922ee6e5c5
supernovatux <thulashitharan.d@gmail.com>
2023-05-25 14:05:05 +0530

    V1
    Added basic builders

--- robo_box_dtp/lib/src/robo_box_dtp_base.dart
+++ robo_box_dtp/lib/src/robo_box_dtp_base.dart
@@ -1,16 +1,104 @@
 // TODO: Put public facing types in this file.
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class RoboCombo {
+  static const String seperator = ',';
+  final List<RoboMoveType> moves;
+  final String name;
+  RoboCombo(this.name, this.moves);
+  @override
+  toString() => moves.length.toString() + seperator + moves.join(seperator);
+}
+
+class RoboComboBuilder {
+  final List<RoboMoveType> moves = [];
+  String name = "Combo${DateTime.now().millisecondsSinceEpoch}";
+  RoboComboBuilder();
+
+  RoboComboBuilder.named(this.name);
+
+  RoboComboBuilder addMove(RoboMoveType move) {
+    moves.add(move);
+    return this;
+  }
+
+  RoboComboBuilder addMoves(List<RoboMoveType> moves) {
+    this.moves.addAll(moves);
+    return this;
+  }
+
+  RoboComboBuilder addName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  RoboCombo build() {
+    if (moves.isEmpty) {
+      throw Exception("No moves added");
+    }
+    return RoboCombo(name, moves);
+  }
 }
 
 enum RoboMoveType {
-  lh(name: "h"),
-  lv(name: "j"),
-  rh(name: "k"),
-  rv(name: "l");
+  lh(name: "H"),
+  lv(name: "J"),
+  rh(name: "K"),
+  rv(name: "L");
 
-  const RoboMoveType({required this.name});
   final String name;
+  const RoboMoveType({required this.name});
+  @override
+  toString() => name;
+}
+
+class RoboState {
+  static const String magic = "==INIT==";
+  static const separator = ':';
+  static const comboSeparator = ';';
+  static const startMagic = "Start";
+  static const endMagic = "Stop";
+  final List<RoboCombo> combos;
+  RoboState(this.combos);
+  String end() => endMagic;
+  String init() =>
+      magic +
+      separator +
+      combos.length.toString() +
+      separator +
+      combos.join(comboSeparator) +
+      comboSeparator;
+
+  String start(int index, double speed) {
+    if (index < 0 || index >= combos.length) {
+      throw Exception("Invalid index");
+    }
+    return startMagic +
+        separator +
+        index.toString() +
+        separator +
+        speed.toString();
+  }
+
+  @override
+  toString() => init();
+}
+
+class RoboStateBuilder {
+  final List<RoboCombo> combos = [];
+  RoboStateBuilder addCombo(RoboCombo combo) {
+    combos.add(combo);
+    return this;
+  }
+
+  RoboStateBuilder addCombos(List<RoboCombo> combos) {
+    this.combos.addAll(combos);
+    return this;
+  }
+
+  RoboState build() {
+    if (combos.isEmpty) {
+      throw Exception("No combos added");
+    }
+    return RoboState(combos);
+  }
 }
--- robo_box_dtp/test/robo_box_dtp_test.dart
+++ robo_box_dtp/test/robo_box_dtp_test.dart
@@ -1,16 +1,67 @@
+import 'dart:math';
+
 import 'package:robo_box_dtp/robo_box_dtp.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  group('Testing the moves enum', () {
+    final move1 = RoboMoveType.lh;
+    final move2 = RoboMoveType.rh;
+    final move3 = RoboMoveType.lv;
+    final move4 = RoboMoveType.rv;
 
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Testing to string', () {
+      expect(move1.toString(), 'H');
+      expect(move2.toString(), 'K');
+      expect(move3.toString(), 'J');
+      expect(move4.toString(), 'L');
+    });
+  });
+  group('Testing the combo builder', () {
+    final comboBuilder = RoboComboBuilder();
+    final combo = comboBuilder
+        .addMove(RoboMoveType.lh)
+        .addMove(RoboMoveType.rh)
+        .addMove(RoboMoveType.lv)
+        .addMove(RoboMoveType.rv)
+        .addName('Test')
+        .build();
+    final comboBuilder2 = RoboComboBuilder();
+    final combo2 = comboBuilder2.addMove(RoboMoveType.lh).build();
+    final comboBuilder3 = RoboComboBuilder();
+    setUp(() {});
+
+    test('Testing to string', () {
+      expect(combo.toString(), '4,H,K,J,L');
+      expect(combo2.toString(), '1,H');
+      expect(() => comboBuilder3.build(), throwsException);
+    });
+  });
+  group('Testing the state builder', () {
+    final stateBuilder = RoboStateBuilder();
+    final comboBuilder = RoboComboBuilder();
+    final combo = comboBuilder
+        .addMove(RoboMoveType.lh)
+        .addMove(RoboMoveType.rh)
+        .addMove(RoboMoveType.lv)
+        .addMove(RoboMoveType.rv)
+        .addName('Test')
+        .build();
+    final comboBuilder2 = RoboComboBuilder();
+    final combo2 = comboBuilder2.addMove(RoboMoveType.lh).build();
+    final state = stateBuilder.addCombo(combo).addCombo(combo2).build();
+    setUp(() {});
+
+    test('Testing to string', () {
+      expect(combo.toString(), '4,H,K,J,L');
+      expect(combo2.toString(), '1,H');
+      expect(state.init(), '==INIT==:2:4,H,K,J,L;1,H;');
+      expect(state.start(1, 10), "Start:1:10.0");
+      expect(state.end(), "Stop");
     });
   });
 }
